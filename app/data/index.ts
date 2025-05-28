87cfeb464a566fe0bba155ae8dcfed71dec965c8
agadzik <andrew.gadzik@vercel.com>
2025-05-28 19:04:27 -0400

    pass cookies to api layer

--- app/data/index.ts
+++ app/data/index.ts
@@ -1,9 +1,17 @@
+import { cookies } from "next/headers";
+
 interface Data {
   status: string;
 }
+
 export async function getData(): Promise<Data> {
   const response = await fetch(
-    "https://community-tracing-example.vercel.app/api/data"
+    "https://community-tracing-example.vercel.app/api/data",
+    {
+      headers: {
+        cookie: (await cookies()).toString(),
+      },
+    }
   );
   const data = (await response.json()) as Data;
   return data;
