12fdb7fa75aeaf9220e58db07b8897482878c856
agadzik <andrew.gadzik@vercel.com>
2025-05-28 18:58:40 -0400

    Add instrumentation and trace api layer

--- app/api/data/route.ts
+++ app/api/data/route.ts
@@ -1,10 +1,16 @@
 import { NextResponse } from "next/server";
+import { trace } from "@opentelemetry/api";
+
+const tracer = trace.getTracer("data");
 
 const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
 export async function GET() {
   for (let i = 0; i < 3; i++) {
-    await sleep(1000); // Sleep for 1 second (1000ms)
+    await tracer.startActiveSpan(`microservice ${i+1}`, async (span) => {
+      await sleep(1000); // Sleep for 1 second (1000ms)
+      span.end();
+    })
   }
   
   return NextResponse.json({ status: 'success' })
