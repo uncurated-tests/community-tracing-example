ffbe4316cf5f16918c8cddd1fa55b87233d0d1bc
agadzik <andrew.gadzik@vercel.com>
2025-05-29 09:31:44 -0400

    Optimize

--- app/api/data/route.ts
+++ app/api/data/route.ts
@@ -6,12 +6,12 @@ const tracer = trace.getTracer("data");
 const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
 export async function GET() {
-  for (let i = 0; i < 3; i++) {
-    await tracer.startActiveSpan(`microservice ${i+1}`, async (span) => {
-      await sleep(1000); // Sleep for 1 second (1000ms)
+  await Promise.allSettled(
+    Array.from({ length: 3 }, (_, i) => tracer.startActiveSpan(`microservice ${i+1}`, async (span) => {
+      await sleep(1000); // 1000 ms = 1 second
       span.end();
-    })
-  }
+    }))
+  )
   
   return NextResponse.json({ status: 'success' })
 }
